0546b8fcaf52fb47a924d8a46754ec1977ec3177
Leah Hullinger <leahhullinger@gmail.com>
2019-08-22 14:35:25 -0600

    date reformatted

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -3,21 +3,25 @@ import StarRatings from "react-star-ratings";
 import styles from "./ReviewCard.module.css";
 
 export default ({ date, body, rating, author }) => {
+  const formatDate = date
+    .substring(0, date.indexOf("T"))
+    .split("-")
+    .reverse()
+    .join("/");
   return (
     <div className={styles.container}>
-      <div className={styles.col_1}>
-        <p>{date}</p>
-        <div>
-          <StarRatings
-            rating={rating}
-            starRatedColor="blue"
-            numberOfStars={6}
-            starDimension="20px"
-          />
-          <p>{rating}</p>
-        </div>
+      <div className={styles.ratingWrapper}>
+        <p>{rating}</p>
+        <StarRatings
+          rating={rating}
+          starRatedColor="blue"
+          numberOfStars={6}
+          starDimension="20px"
+          starSpacing="3px"
+        />
       </div>
-      <div className={styles.col_2}>
+      <div className={styles.reviewWrapper}>
+        <p>{formatDate}</p>
         <p>"{body}"</p>
         <p>- {author}</p>
       </div>
