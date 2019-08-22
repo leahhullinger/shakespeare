d8cdd396cb4687a08d1c626531f0848b4f13f5b4
Leah Hullinger <leahhullinger@gmail.com>
2019-08-22 13:50:52 -0600

    star rating working

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import StarRatings from "react-star-ratings";
 import styles from "./ReviewCard.module.css";
 
 export default ({ date, body, rating, author }) => {
@@ -6,10 +7,18 @@ export default ({ date, body, rating, author }) => {
     <div className={styles.container}>
       <div className={styles.col_1}>
         <p>{date}</p>
-        <div>{rating}</div>
+        <div>
+          <StarRatings
+            rating={rating}
+            starRatedColor="blue"
+            numberOfStars={6}
+            starDimension="20px"
+          />
+          <p>{rating}</p>
+        </div>
       </div>
       <div className={styles.col_2}>
-        <p>{body}</p>
+        <p>"{body}"</p>
         <p>- {author}</p>
       </div>
     </div>
