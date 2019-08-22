7c873ddbb190877eddecba42a7b3ee97cb7d3f1d
Leah Hullinger <leahhullinger@gmail.com>
2019-08-22 11:16:07 -0600

    basic ReviewCard set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AppHeader from "./components/AppHeader/AppHeader";
+import ReviewCard from "./components/ReviewCard/ReviewCard";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <ReviewCard />
     </div>
   );
 }
---
+++ src/components/ReviewCard/ReviewCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./ReviewCard.module.css";
+
+export default ({ date, body, rating, author }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.col_1}>
+        <p>{date}</p>
+        <div>star rating</div>
+      </div>
+      <div className={styles.col_2}>
+        <p>{body}</p>
+        <p>{author}</p>
+      </div>
+    </div>
+  );
+};
