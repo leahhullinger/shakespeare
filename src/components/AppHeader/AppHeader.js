c53240a6e3cbe7c9ca88a1c5ff80a55f0be27368
Leah Hullinger <leahhullinger@gmail.com>
2019-08-22 10:46:58 -0600

    created AppHeader

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./AppHeader.module.css";
+
+export default () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Shakespeare, oh Shakespeare!</h2>
+    </div>
+  );
+};
